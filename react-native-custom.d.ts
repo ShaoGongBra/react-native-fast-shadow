4a2cf37009dfd13ef98c96d3a51a170c419eb8b8
eslamvet1 <58374613+eslamvet@users.noreply.github.com>
2025-01-06 12:52:14 +0300

    Update react-native-custom.d.ts

--- react-native-custom.d.ts
+++ react-native-custom.d.ts
@@ -1,3 +1,9 @@
 declare module 'react-native/Libraries/Utilities/codegenNativeComponent' {
     export default function codegenNativeComponent<P>(name: string): React.ComponentType<P>;
 }
+
+declare module 'react-native/Libraries/Types/CodegenTypes' {
+    export type Int32 = number;
+    export type Float = number;
+    export type WithDefault<T, D> = T | D;
+}
